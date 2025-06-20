1afc1d309b143bb96ba021c7a0193df14eadc9c4
Lucas R <lrassbachson@gmail.com>
2025-06-20 18:22:24 +0100

    mpu updatesx

--- raspberrypi_code/src/basic_mpu.py
+++ raspberrypi_code/src/basic_mpu.py
@@ -7,20 +7,19 @@ class BasicMPU:
 
     def collect_bias(self):
         time.sleep(5)
-        accel_bias, gyro_bias, temperature = self.read_sensor_data()
-        print(accel_bias)
-        print(gyro_bias)
+        self.accel_bias, self.gyro_bias, temperature = self.read_sensor_data()
 
     # Define a function to read the sensor data
-    def read_sensor_data():
+    def read_sensor_data(self):
         # Read the accelerometer values
-        accelerometer_data = mpu6050.get_accel_data()
+        accelerometer_data = self.mpu.get_accel_data()
+	accelerometr_data["x"] -= self.accel_bias["x"]
 
         # Read the gyroscope values
-        gyroscope_data = mpu6050.get_gyro_data()
+        gyroscope_data = self.mpu.get_gyro_data()
 
         # Read temp
-        temperature = mpu6050.get_temp()
+        temperature = self.mpu.get_temp()
 
         return accelerometer_data, gyroscope_data, temperature
     
