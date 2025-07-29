b986ecb7b64fe67fd9bec505482070bcd56c6b70
Lucas <lrassbachson@gmail.com>
2025-07-29 13:35:15 -0700

    implement controls on MC, updated testing script

--- raspberrypi_code/src/motorcontroller.py
+++ raspberrypi_code/src/motorcontroller.py
@@ -5,24 +5,53 @@ class MotorController:
     # Set serial port and baud rate
     self.port = '/dev/ttyACM0'
     self.baud_rate = 9600
-    self.zero_message = "" # this will need to be defined as the initial/final message that sets servos to 0
+    self.zero_message = "0,0,0,0" # this will need to be defined as the initial/final message that sets servos to 0
+    self.max_speed = 30
 
   def start(self): 
     # Open the serial port
     self.ser = serial.Serial(self.port, self.baud_rate)
-    self.ser.write(self.zero_message)
+    self.send(self.zero_message)
 
   def stop(self):
-    self.ser.write(self.zero_message)
+    self.send(self.zero_message)
     self.ser.close()
 
-  def send(self, input):
-    #  TODO define how messages will be sent and processed. json in? delimited list in a string out?
-    processed_string = str(input).encode() + b'\n'
-    self.ser.write()
+  def validate_message(self,msg):
+    if(msg == "quit"):
+      return "quit"
+    signal_in = msg.split(",")
+    signal_out_arr = []
+    len_signal = len(signal_in)
+    if(len_signal != 4):
+      print(f"BAD COMMAND FORMAT (missing a comma len=={len_signal}); {msg} not sent")
+      return None
+    try:
+      for i in range(4):
+        if(int(signal_in[i]) > self.max_speed):
+          print(f"CAPPED SPEED at command position: {i}")
+          signal_out_arr.append(str(self.max_speed))
+        elif(int(signal_in[i]) < 0):
+          print(f"NEGATIVE ENCOUNTERED: {i}; {msg} not sent")
+          return None
+        else:
+          signal_out_arr.append(signal_in[i])
+      output = ",".join(signal_out_arr)
+    except Exception as e:
+      print(f"exception: {e}")
+      print(f"{msg} not sent")
+      return None
+    return output
 
-  # TODO this logic needs to be added outside of the class by the caller
-  #except Exception:
-  #    ser.write(str(0) + b'\n')
-  #    ser.close()
-  #    print("Serial port closed.")
+  def send(self, input):
+    validated_message = self.validate_message(input)
+    if validated_message is not None:
+      processed_string = str(validated_message).encode() + b'\n'
+      self.ser.write(processed_string)
+    else:
+      print("No message sent")
+
+if __name__ == "__main__":
+  mc = MotorController()
+  msg = mc.validate_message("50,0,0,0")
+  print(msg)
--- raspberrypi_code/src/test_serial_motor.py
+++ raspberrypi_code/src/test_serial_motor.py
@@ -1,27 +1,15 @@
-import serial
 
-# Set serial port and baud rate
-serial_port = '/dev/ttyACM0'  # Or /dev/ttyUSB0, etc.
-baud_rate = 9600
+import motorcontroller
 
-# Open the serial port
-ser = serial.Serial(serial_port, baud_rate)
 
 try:
+  mc = motorcontroller.MotorController()
+  mc.start()
   while True:
     # Get input from the user (example)
     message = input("Enter message to send: ")
-    if(message == "quit"):
-        ser.write(str("0,0,0,0").encode() + b'\n') 
-        ser.close()
-        print("Serial port closed.")
-        quit()
-    
-    # Encode the message and send it to the Arduino
-    ser.write(message.encode() + b'\n') 
-    print("Sent:", message)
+    mc.send(message)
     
 except KeyboardInterrupt:
-    ser.write(str("0,0,0,0") + b'\n') 
-    ser.close()
+    mc.stop()
     print("Serial port closed.")
